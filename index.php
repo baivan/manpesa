d923e6a3a084f1e9ac2ae2d4d741dadb984b1e92
Southwell Tech <tech@southwell.io>
2017-05-03 17:10:57 +0300

    summary

--- index.php
+++ index.php
@@ -96,6 +96,7 @@ $sale_route ->get('all','getSales');
 $sale_route ->post('crm/all','getTableSales'); 
 $sale_route ->get('crm/all','getTableSales');
 $sale_route ->post('summary','dashBoardSummary');
+$sale_route ->get('summary','dashBoardSummary');
 
 $category_route = new MicroCollection();
 $category_route ->setPrefix('/category/');
