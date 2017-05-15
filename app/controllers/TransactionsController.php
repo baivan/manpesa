20f6e1628020c23cb3a44f42683950b92a4aed1a
Southwell Tech <tech@southwell.io>
2017-05-15 06:07:27 +0300

    check payment

--- app/controllers/TransactionsController.php
+++ app/controllers/TransactionsController.php
@@ -72,6 +72,15 @@ class TransactionsController extends Controller {
             $sale = Sales::findFirst(array("salesID=:id: ",
                         'bind' => array("id" => $salesID)));
 
+            $saleQuery ="SELECT s.salesID FROM transaction t JOIN contacts c on t.salesID=c.nationalIdNumber or t.salesID=c.workMobile JOIN customer cu on c.contactsID=cu.contactsID JOIN sales s on cu.customerID=s.customerID where c.nationalIdNumber='%$salesID%' or c.workMobile='%$salesID%'";
+            if(!$sale){
+                $mappedSale = $this->rawSelect($saleQuery);
+
+               $salesID=$mappedSale[0]['salesID'];
+                $sale = Sales::findFirst(array("salesID=:id: ",
+                        'bind' => array("id" => $salesID)));
+            }
+
 
             if ($sale) {
                 $sale->status = $this->salePaid;
@@ -124,15 +133,6 @@ class TransactionsController extends Controller {
         $token = $json->token;
         //  $userID = $json->userID;
         $salesID = $json->salesID;
-
-
-        // $isPaid = $this->checkSalePaid($salesID);
-        
-
-
-         //join sales s on cu.customerID=s.customerID 
-
-
         
 
         $getAmountQuery="SELECT SUM(replace(t.depositAmount,',','')) as amount, s.amount as saleAmount, st.salesTypeDeposit,si.saleItemID,i.serialNumber,i.status as itemStatus from transaction t join contacts c on t.salesID=c.workMobile or t.salesID=c.nationalIdNumber join customer cu on c.contactsID=cu.contactsID join sales s on cu.customerID=s.customerID JOIN payment_plan pp on s.paymentPlanID=pp.paymentPlanID join sales_type st on pp.salesTypeID=st.salesTypeID left join sales_item si on t.salesID=si.saleID left join item i on si.itemID=i.itemID where s.salesID=$salesID ";
